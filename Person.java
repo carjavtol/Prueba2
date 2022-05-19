28d577de15423c1cb67bd6919234d9b6b9859f5d
Carla <carjavtol@gmail.com>
2022-05-19 15:39:27 +0200

    Initial commit

---
+++ Person.java
@@ -0,0 +1,17 @@
+package data;
+
+public class Person
+{
+    private String name;
+
+    public Person(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+}
+
